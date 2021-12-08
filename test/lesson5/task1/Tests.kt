c6689f67487d617ea8b2127f344823f2b5bb7302
Pertseva Evgenia <perceva2003@bk.ru>
2021-12-08 18:39:15 +0300

    Начал делать урок 7

--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -240,6 +240,7 @@ class Tests {
         assertFalse(canBuildFrom(emptyList(), "foo"))
         assertTrue(canBuildFrom(listOf('a', 'b', 'o'), "baobab"))
         assertFalse(canBuildFrom(listOf('a', 'm', 'r'), "Marat"))
+        assertFalse(canBuildFrom(listOf(), "]"))
     }
 
     @Test
--- test/lesson6/task1/Tests.kt
+++ test/lesson6/task1/Tests.kt
@@ -89,7 +89,7 @@ class Tests {
     fun bestHighJump() {
         assertEquals(226, bestHighJump("226 +"))
         assertEquals(-1, bestHighJump("???"))
-        assertEquals(-1, bestHighJump("220 + 224 .. 228 %- 230 + 232 %%- 234 %"))
+        assertEquals(-1, bestHighJump("220 + 224 -2- 228 %- 230 + 232 %%- 234 %"))
         assertEquals(230, bestHighJump("220 + 224 %+ 228 %- 230 + 232 %%- 234 %"))
     }
 
